daa5c487b561a6b7437929c16dd1a1a467ae13e7
raavenkkj <grupodoruim@gmail.com>
2023-02-28 22:49:48 -0300

    mensagem descritiva do commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeAreDevs API v1 CSharp")]
+[assembly: AssemblyDescription("Used to build software accessing the Lua C API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeAreDevs_API.Properties")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6d8a8bf5-f458-403a-a65f-8989c36536fd")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ _003CModule_003E.cs
@@ -0,0 +1,12 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: <Module>
+// Assembly: WeAreDevs_API, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 70EDAC8E-5221-42DB-9AEB-B82E4E8461FA
+// Assembly location: D:\Documentos\Downloads\WeAreDevs_API.dll
+
+using Costura;
+
+internal class \u003CModule\u003E
+{
+  static \u003CModule\u003E() => AssemblyLoader.Attach();
+}
