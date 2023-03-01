0e3a920caff5f712ac42c8e7abb126454640bfdc
raven <78323525+raavenkkj@users.noreply.github.com>
2023-02-28 22:57:11 -0300

    Update WeAreDevs_API_ProcessedByFody.cs

--- WeAreDevs_API_ProcessedByFody.cs
+++ WeAreDevs_API_ProcessedByFody.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: WeAreDevs_API_ProcessedByFody
-// Assembly: WeAreDevs_API, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 70EDAC8E-5221-42DB-9AEB-B82E4E8461FA
-// Assembly location: D:\Documentos\Downloads\WeAreDevs_API.dll
-
 internal class WeAreDevs_API_ProcessedByFody
 {
   internal const string FodyVersion = "6.5.5.0";
